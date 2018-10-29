d6a2e9636e716654063b60d7b2de1d697916291b
arnol8725 <arnol87.peralta@gmail.com>
2018-10-29 10:52:58 -0600

    commit 29102018

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -24,3 +24,21 @@ INSERT INTO clientes (nombre, apellido, email, create_at,foto) VALUES('John', 'S
 INSERT INTO clientes (nombre, apellido, email, create_at,foto) VALUES('Joe', 'Bloggs', 'joe.bloggs@gmail.com', '2017-08-23','');
 INSERT INTO clientes (nombre, apellido, email, create_at,foto) VALUES('John', 'Stiles', 'john.stiles@gmail.com', '2017-08-24','');
 INSERT INTO clientes (nombre, apellido, email, create_at,foto) VALUES('Richard', 'Roe', 'stiles.roe@gmail.com', '2017-08-25','');
+
+/* Populate tabla productos */
+INSERT INTO productos (nombre, precio, create_at) VALUES('Panasonic Pantalla LCD', 259990, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('Sony Camara digital DSC-W320B', 123490, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('Apple iPod shuffle', 1499990, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('Sony Notebook Z110', 37990, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('Hewlett Packard Multifuncional F2280', 69990, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('Bianchi Bicicleta Aro 26', 69990, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('Mica Comoda 5 Cajones', 299990, NOW());
+
+insert into facturas(descripcion,observacion,cliente_id,create_at) values('Factura equipos de oficina',null,1,NOW());
+insert into facturas_items(cantidad,factura_id,producto_id) values (1,1,1);
+insert into facturas_items(cantidad,factura_id,producto_id) values (2,1,4);
+insert into facturas_items(cantidad,factura_id,producto_id) values (1,1,5);
+insert into facturas_items(cantidad,factura_id,producto_id) values (1,1,6);
+
+insert into facturas(descripcion,observacion,cliente_id,create_at) values('Factura Bicicleta','Alguna nota importante',1,NOW());
+insert into facturas_items(cantidad,factura_id,producto_id) values (3,2,6);
